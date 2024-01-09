f0d88282e4791ed65f4abc74fc04ed3755645161
Adriel <adriel.fsantos@outlook.com>
2024-01-09 00:56:08 -0300

    feat: Add widget ShowPasswordIconButton

---
+++ lib/screens/auth/widgets/show_password_icon_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/themes/styles/colors.dart';
+
+class ShowPasswordIconButton extends StatelessWidget {
+  final bool showPassword;
+  final VoidCallback onPressed;
+
+  const ShowPasswordIconButton(
+      {super.key, required this.onPressed, required this.showPassword});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(
+        showPassword
+            ? Icons.visibility_outlined
+            : Icons.visibility_off_outlined,
+        color: primary,
+      ),
+    );
+  }
+}
