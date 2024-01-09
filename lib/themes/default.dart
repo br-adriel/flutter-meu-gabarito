23e1d085165e7848a6ed20bb8b3b52adc705059d
Adriel <adriel.fsantos@outlook.com>
2024-01-09 00:55:28 -0300

    feat: Add more properties to default theme

--- lib/themes/default.dart
+++ lib/themes/default.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meu_gabarito/themes/colors.dart';
+import 'package:meu_gabarito/themes/styles/colors.dart';
+import 'package:meu_gabarito/themes/styles/button_styles.dart';
 
 ThemeData defaultTheme = ThemeData(
-  textTheme: GoogleFonts.nunitoSansTextTheme(),
+  textTheme: GoogleFonts.exo2TextTheme(),
   primarySwatch: primary,
+  textSelectionTheme: TextSelectionThemeData(
+    cursorColor: primary,
+    selectionColor: primary[100],
+    selectionHandleColor: primary,
+  ),
+  filledButtonTheme: FilledButtonThemeData(style: filledButtonStyle),
 );
