03133da9fcb2d1a3cafec8d019b9b18e52a1daf3
Adriel <adriel.fsantos@outlook.com>
2024-01-08 23:29:09 -0300

    feat: Add default theme

---
+++ lib/themes/default.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meu_gabarito/themes/colors.dart';
+
+ThemeData defaultTheme = ThemeData(
+  textTheme: GoogleFonts.nunitoSansTextTheme(),
+  primarySwatch: primary,
+);
