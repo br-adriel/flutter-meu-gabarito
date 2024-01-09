88a63c1334fa72acf363edbf17ecc0fd58ab45ad
Adriel <adriel.fsantos@outlook.com>
2024-01-09 12:38:53 -0300

    feat: Setup flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:meu_gabarito/firebase_options.dart';
 import 'package:meu_gabarito/screens/auth/login.dart';
 import 'package:meu_gabarito/themes/index.dart' as themes;
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
