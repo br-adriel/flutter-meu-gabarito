9a10ddda8be79ee0cefc2134600a126f8d34d2ab
Adriel <adriel.fsantos@outlook.com>
2024-01-09 00:57:07 -0300

    feat: Add login screen

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/screens/auth/widgets/auth_card.dart';
+import 'package:meu_gabarito/screens/auth/widgets/show_password_icon_button.dart';
+import 'package:meu_gabarito/themes/styles/text_styles.dart';
+import 'package:meu_gabarito/themes/styles/textfield_styles.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthCard(
+      image: "assets/img/study-girl-book.jpg",
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "meuGabarito",
+            style: logoTextStyle,
+          ),
+          const Text("Continue seus estudos de qualquer lugar"),
+          const SizedBox(height: 16),
+          Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: textFieldInputDecoration("Email"),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: textFieldInputDecoration("Senha").copyWith(
+                    suffixIcon: ShowPasswordIconButton(
+                      onPressed: () {
+                        setState(() {
+                          _showPassword = !_showPassword;
+                        });
+                      },
+                      showPassword: _showPassword,
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.go,
+                  obscureText: !_showPassword,
+                ),
+                const SizedBox(height: 8),
+                FilledButton(
+                  onPressed: () {},
+                  child: const Text("Entrar"),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
