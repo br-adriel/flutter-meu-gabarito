0ed0889f335ef8b081433ab8f1e962d6ceb63c9f
Adriel <adriel.fsantos@outlook.com>
2024-01-09 17:35:12 -0300

    feat: Add color variation for when input is invalid

--- lib/themes/styles/textfield_styles.dart
+++ lib/themes/styles/textfield_styles.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meu_gabarito/themes/styles/colors.dart';
 
-InputDecoration textFieldInputDecoration(String labelText) {
+InputDecoration textFieldInputDecoration(
+  String labelText, {
+  bool isValid = true,
+}) {
   return InputDecoration(
     border: UnderlineInputBorder(
       borderRadius: BorderRadius.circular(8),
       borderSide: BorderSide.none,
     ),
     labelText: labelText,
-    fillColor: primary[50],
+    fillColor: isValid ? primary[50] : Colors.red[50],
     filled: true,
     contentPadding: const EdgeInsets.fromLTRB(12, 8, 12, 12),
-    labelStyle: const TextStyle(color: primary),
+    labelStyle: TextStyle(color: isValid ? primary : Colors.red[900]),
   );
 }
