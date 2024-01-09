8c8dba8bbcd5f8662c7711e201696d4867421095
Adriel <adriel.fsantos@outlook.com>
2024-01-09 17:35:51 -0300

    feat: Add login form fields validators

---
+++ lib/screens/auth/validators/login_form.dart
@@ -0,0 +1,15 @@
+import 'package:meu_gabarito/validators/email_validator.dart';
+import 'package:meu_gabarito/validators/length_validator.dart';
+import 'package:meu_gabarito/validators/required.dart';
+
+String? validatePasswordField(String? value) {
+  String? validationResult = validateRequired(value);
+  if (validationResult is String) return validationResult;
+  return validateLength(value, min: 8);
+}
+
+String? validateEmailField(String? value) {
+  String? validationResult = validateRequired(value);
+  if (validationResult is String) return validationResult;
+  return validateEmail(value);
+}
