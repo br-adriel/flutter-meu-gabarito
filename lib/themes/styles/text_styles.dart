c5336dd7928130057a9a3151a3a5b944c72205d0
Adriel <adriel.fsantos@outlook.com>
2024-01-10 23:33:56 -0300

    feat: Add appBarLogoTextStyle

--- lib/themes/styles/text_styles.dart
+++ lib/themes/styles/text_styles.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 TextStyle logoTextStyle = const TextStyle(fontSize: 32);
+
+TextStyle appBarLogoTextStyle = GoogleFonts.exo2().copyWith(fontSize: 18);
