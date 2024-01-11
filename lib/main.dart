722467da165998ed72089c980b11c6f5ff232dfa
Adriel <adriel.fsantos@outlook.com>
2024-01-10 22:17:03 -0300

    feat: Setup app store

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:meu_gabarito/firebase_options.dart';
 import 'package:meu_gabarito/screens/auth/login.dart';
+import 'package:meu_gabarito/store/main.dart';
 import 'package:meu_gabarito/themes/index.dart' as themes;
+import 'package:provider/provider.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final _providers = [Provider<MainStore>(create: (_) => MainStore())];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
-      theme: themes.defaultTheme,
+    return MultiProvider(
+      providers: _providers,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Observer(builder: (context) {
+          var store = Provider.of<MainStore>(context);
+          if (store.auth.user == null) return const LoginScreen();
+          return const Scaffold();
+        }),
+        theme: themes.defaultTheme,
+      ),
     );
   }
 }
