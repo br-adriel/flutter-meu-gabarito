e22b2336dc025daa17046a2f8a7025cb21e8887e
Adriel <adriel.fsantos@outlook.com>
2024-01-12 00:10:17 -0300

    feat: Remove appBarLogoTextStyle

--- lib/themes/styles/text_styles.dart
+++ lib/themes/styles/text_styles.dart
@@ -1,10 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:meu_gabarito/themes/styles/colors.dart';
 
 TextStyle logoTextStyle = const TextStyle(fontSize: 32);
-
-TextStyle appBarLogoTextStyle = GoogleFonts.exo2().copyWith(
-  fontSize: 22,
-  color: primary,
-);
