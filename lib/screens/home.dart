6492bf3d18c224ac7077118159dba00b83b613f9
Adriel <adriel.fsantos@outlook.com>
2024-01-11 23:42:53 -0300

    refactor: Update HomeScreen with DefaultAppbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:meu_gabarito/store/main.dart';
-import 'package:meu_gabarito/themes/styles/colors.dart';
-import 'package:meu_gabarito/themes/styles/text_styles.dart';
-import 'package:provider/provider.dart';
+import 'package:meu_gabarito/widgets/appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var auth = Provider.of<MainStore>(context).auth;
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primary,
-        centerTitle: true,
-        title: const Text("meuGabarito"),
-        titleTextStyle: appBarLogoTextStyle,
-        actions: [
-          IconButton(
-            onPressed: auth.logout,
-            icon: const Icon(Icons.output_rounded),
-            color: Colors.white,
-            tooltip: "Sair",
-          ),
-        ],
-      ),
+    return const Scaffold(
+      appBar: DefaultAppbar(),
     );
   }
 }
