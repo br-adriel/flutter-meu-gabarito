9456464ba36c28efd4f2fd903321061de314ce32
Adriel <adriel.fsantos@outlook.com>
2024-01-12 00:13:11 -0300

    refactor: Add LogoAppBar to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meu_gabarito/widgets/appbar.dart';
+import 'package:meu_gabarito/widgets/logo_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: DefaultAppbar(),
+      appBar: LogoAppBar(),
     );
   }
 }
