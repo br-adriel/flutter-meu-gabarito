d10302541a2df9927d636540e05f3896c1f223c7
Adriel <adriel.fsantos@outlook.com>
2024-01-11 23:41:51 -0300

    feat: Add file for profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/themes/styles/colors.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: primary,
+    );
+  }
+}
