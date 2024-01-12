da5d2c11eeec6fa29043aab9945e07f7e39a9d96
Adriel <adriel.fsantos@outlook.com>
2024-01-12 00:12:03 -0300

    feat: Add AppBar on profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:meu_gabarito/themes/styles/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: primary,
+    return Scaffold(
+      appBar: AppBar(title: const Text("Seu perfil")),
     );
   }
 }
