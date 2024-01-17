3ae6120eb2366d8eabc4d8a33e02e78b417b93a8
Adriel <adriel.fsantos@outlook.com>
2024-01-16 22:25:51 -0300

    feat: Add widget UserProfileImage

---
+++ lib/widgets/user_profile_image.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/store/main.dart';
+import 'package:meu_gabarito/themes/styles/colors.dart';
+import 'package:provider/provider.dart';
+
+class UserProfileImage extends StatelessWidget {
+  final double radius;
+
+  const UserProfileImage({super.key, this.radius = 20});
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = Provider.of<MainStore>(context).auth.user;
+
+    return CircleAvatar(
+      radius: radius,
+      backgroundColor: primary[200],
+      foregroundColor: primary[50],
+      backgroundImage: user != null && user.photoURL != null
+          ? NetworkImage(user.photoURL!)
+          : null,
+      child: user != null && user.photoURL == null
+          ? Icon(Icons.person_rounded, size: radius)
+          : null,
+    );
+  }
+}
