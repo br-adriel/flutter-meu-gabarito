9b4c2ec5f8bf8033c65d85ff35fb03c2ab611b32
Adriel <adriel.fsantos@outlook.com>
2024-01-26 17:18:44 -0300

    feat: Add widget UpdateUserNameBottomSheet

---
+++ lib/screens/profile/widgets/update_user_name_bottom_sheet.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/store/auth/auth.dart';
+import 'package:meu_gabarito/store/main.dart';
+import 'package:meu_gabarito/themes/styles/text_styles.dart';
+import 'package:meu_gabarito/themes/styles/textfield_styles.dart';
+import 'package:provider/provider.dart';
+
+class UpdateUserNameBottomSheet extends StatelessWidget {
+  const UpdateUserNameBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Auth auth = Provider.of<MainStore>(context).auth;
+    var textController =
+        TextEditingController(text: auth.user?.displayName ?? '');
+    textController.selection =
+        TextSelection.collapsed(offset: textController.text.length);
+
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Text(
+              "Alterar nome",
+              style: titleTextStyle,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            TextField(
+              controller: textController,
+              decoration: textFieldInputDecoration("Nome"),
+              textInputAction: TextInputAction.done,
+            ),
+            FilledButton(
+              onPressed: () async {
+                auth.updateName(textController.text.trim());
+                Navigator.of(context).pop();
+              },
+              child: const Text("Atualizar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
