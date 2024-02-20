6d33e924bce84934317a45cb3e591616260ed4fe
Adriel <adriel.fsantos@outlook.com>
2024-02-20 18:10:10 -0300

    feat: Improve textFieldInputDecoration function

--- lib/themes/styles/textfield_styles.dart
+++ lib/themes/styles/textfield_styles.dart
@@ -4,17 +4,13 @@ import 'package:meu_gabarito/themes/styles/colors.dart';
 InputDecoration textFieldInputDecoration(
   String labelText, {
   bool isValid = true,
+  String? helperText,
 }) {
   return InputDecoration(
-    border: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
-      borderSide: BorderSide.none,
-    ),
     labelText: labelText,
     fillColor: isValid ? primary[50] : Colors.red[50],
-    filled: true,
-    contentPadding: const EdgeInsets.fromLTRB(12, 8, 12, 12),
     labelStyle: TextStyle(color: isValid ? primary : Colors.red[900]),
+    helperText: helperText,
   );
 }
 
