5a145c963235235982b89e596689a2b324df2b18
Adriel <adriel.fsantos@outlook.com>
2024-02-20 18:24:17 -0300

    feat: Add bottomSheetThemeData

---
+++ lib/themes/styles/bottom_sheet_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const bottomSheetThemeData = BottomSheetThemeData(
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+);
