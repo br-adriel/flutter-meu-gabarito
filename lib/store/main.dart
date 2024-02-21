5474088d700876d9979b056576130b5d3f953f05
Adriel <adriel.fsantos@outlook.com>
2024-02-20 21:39:35 -0300

    feat: Add Gabaritos to main store

--- lib/store/main.dart
+++ lib/store/main.dart
@@ -1,4 +1,5 @@
 import 'package:meu_gabarito/store/auth/auth.dart';
+import 'package:meu_gabarito/store/gabaritos/gabaritos.dart';
 import 'package:mobx/mobx.dart';
 
 part 'main.g.dart';
@@ -7,4 +8,5 @@ class MainStore = _MainStore with _$MainStore;
 
 abstract class _MainStore with Store {
   final auth = Auth();
+  final gabaritos = Gabaritos();
 }
