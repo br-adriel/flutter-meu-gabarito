162cc22927ddcda4aa3159ad35ba84cd446b5d83
Adriel <adriel.fsantos@outlook.com>
2024-02-20 22:12:00 -0300

    feat: Update Gabarito class to handle firebase

--- lib/classes/gabarito.dart
+++ lib/classes/gabarito.dart
@@ -1,22 +1,56 @@
-import 'package:meu_gabarito/classes/questao.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Gabarito {
-  String id;
-  String nome;
-  List<Questao> questoes;
+  String? id;
+  String? nome;
+  int? contagemCorrigidas;
+  int? contagemCorretas;
+  int? tamanho;
 
-  Gabarito({required this.id, required this.nome, required this.questoes});
+  Gabarito({
+    required this.id,
+    required this.nome,
+    required this.tamanho,
+    required this.contagemCorretas,
+    required this.contagemCorrigidas,
+  });
 
-  bool get corrigido => questoes.every((q) => q.corrigida);
+  int get porcentagemCorrigidas {
+    if (contagemCorrigidas == null || tamanho == null) return 0;
+    return (contagemCorrigidas! ~/ tamanho!) * 100;
+  }
 
-  int get contagemCorrigidas => questoes.where((q) => q.corrigida).length;
+  int get porcentagemCorretas {
+    if (contagemCorretas == null || contagemCorrigidas == null) return 0;
+    return contagemCorretas! ~/ contagemCorrigidas! * 100;
+  }
 
-  int get porcentagemCorrigidas =>
-      (contagemCorrigidas ~/ questoes.length) * 100;
+  bool get corrigido {
+    if (contagemCorrigidas == null || tamanho == null) return false;
+    return contagemCorrigidas == tamanho;
+  }
 
-  int get contagemAcertadas => questoes
-      .where((q) => q.alternativaCorreta == q.alternativaSelecionada)
-      .length;
+  factory Gabarito.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Gabarito(
+      id: data?['id'],
+      nome: data?['nome'],
+      contagemCorretas: data?['contagemCorretas'],
+      contagemCorrigidas: data?['contagemCorrigidas'],
+      tamanho: data?['tamanho'],
+    );
+  }
 
-  int get porcentagemAcertadas => contagemAcertadas ~/ contagemCorrigidas;
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (id != null) 'id': id,
+      if (nome != null) 'nome': nome,
+      if (contagemCorretas == null) 'contagemCorretas': contagemCorretas,
+      if (contagemCorrigidas == null) 'contagemCorrigidas': contagemCorrigidas,
+      if (tamanho == null) 'tamanho': tamanho,
+    };
+  }
 }
