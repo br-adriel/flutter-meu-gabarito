c2d3260636546adcc6b8118eb8a98d8122dbad57
Adriel <adriel.fsantos@outlook.com>
2024-02-22 16:01:16 -0300

    feat: Add title2TextStyle

--- lib/themes/styles/text_styles.dart
+++ lib/themes/styles/text_styles.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 TextStyle logoTextStyle = const TextStyle(fontSize: 32);
 
 TextStyle titleTextStyle = const TextStyle(fontSize: 24);
+
+TextStyle title2TextStyle = const TextStyle(fontSize: 20);
