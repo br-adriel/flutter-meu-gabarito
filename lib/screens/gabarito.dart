8fee94cddc5ed4ec369797abfa41ab26a050aa66
Adriel <adriel.fsantos@outlook.com>
2024-02-24 22:14:49 -0300

    feat: Add GabaritoScreen

---
+++ lib/screens/gabarito.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:meu_gabarito/classes/gabarito.dart';
+import 'package:meu_gabarito/store/gabaritos/gabaritos.dart';
+import 'package:meu_gabarito/store/main.dart';
+import 'package:meu_gabarito/widgets/gabarito_actions.dart';
+import 'package:meu_gabarito/widgets/gabarito_stats.dart';
+import 'package:meu_gabarito/widgets/logo_appbar.dart';
+import 'package:meu_gabarito/widgets/questao_card.dart';
+import 'package:provider/provider.dart';
+
+class GabaritoScreen extends HookWidget {
+  final Gabarito gabarito;
+
+  const GabaritoScreen({super.key, required this.gabarito});
+
+  @override
+  Widget build(BuildContext context) {
+    Gabaritos store = Provider.of<MainStore>(context).gabaritos;
+
+    useEffect(() {
+      final listeners = store.getGabarito(gabarito.id!);
+      return () {
+        for (var listener in listeners) {
+          listener.cancel();
+        }
+      };
+    }, []);
+
+    return Scaffold(
+      appBar: LogoAppBar(centerTitle: true, title: gabarito.nome!),
+      body: store.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Observer(
+              builder: (context) => ListView(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 16),
+                children: [
+                  GabaritoStats(
+                    tamanho: gabarito.tamanho!,
+                    acertos: gabarito.contagemCorretas!,
+                    corrigidas: gabarito.contagemCorrigidas!,
+                    taxaAcerto: gabarito.porcentagemCorretas,
+                    taxaCorrecao: gabarito.porcentagemCorrigidas,
+                  ),
+                  const SizedBox(height: 12),
+                  const GabaritoActions(),
+                  const SizedBox(height: 12),
+                  ...store.questoes.map((q) => QuestaoCard(q)),
+                ],
+              ),
+            ),
+    );
+  }
+}
