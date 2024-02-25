ac1a9b80040aac14204806a27d5ee54c1c109595
Adriel <adriel.fsantos@outlook.com>
2024-02-25 20:00:04 -0300

    feat: Add rename gabarito dialog

--- lib/widgets/gabarito_actions.dart
+++ lib/widgets/gabarito_actions.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:meu_gabarito/classes/gabarito.dart';
 import 'package:meu_gabarito/store/gabaritos/gabaritos.dart';
 import 'package:meu_gabarito/store/main.dart';
 import 'package:meu_gabarito/themes/styles/button_styles.dart';
+import 'package:meu_gabarito/validators/required.dart';
 import 'package:provider/provider.dart';
 
-class GabaritoActions extends StatelessWidget {
-  final Gabarito gabarito;
+Widget cancelButton(BuildContext context) => TextButton(
+      onPressed: () => Navigator.of(context).pop(),
+      child: const Text('Cancelar'),
+    );
 
-  const GabaritoActions(this.gabarito, {super.key});
+void handlePromise(Future<void> promise, BuildContext context, Gabaritos store,
+    {int popAmout = 2}) {
+  int count = 0;
+  promise.then((_) {
+    Navigator.of(context).popUntil((_) => count++ >= popAmout);
+  }).catchError((_) {
+    store.errors.map((err) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(err)),
+      );
+    });
+    Navigator.of(context).pop();
+  });
+}
+
+class GabaritoActions extends HookWidget {
+  final Gabarito gabarito;
+  final renameTC = TextEditingController();
+
+  GabaritoActions(this.gabarito, {super.key});
 
   @override
   Widget build(BuildContext context) {
     Gabaritos store = Provider.of<MainStore>(context).gabaritos;
+
+    useEffect(() {
+      renameTC.text = gabarito.nome!;
+      return null;
+    });
 
     return Row(
       children: [
@@ -33,7 +61,41 @@ class GabaritoActions extends StatelessWidget {
         const SizedBox(width: 8),
         Expanded(
           child: FilledButton(
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Renomear gabarito'),
+                  content: TextFormField(
+                    controller: renameTC,
+                    validator: validateRequired,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  actions: [
+                    cancelButton(context),
+                    FilledButton(
+                      onPressed: () {
+                        if (validateRequired(renameTC.text) == null) {
+                          handlePromise(
+                            store.renameGabarito(gabarito.id!, renameTC.text),
+                            context,
+                            store,
+                            popAmout: 1,
+                          );
+                        }
+                      },
+                      child: const Text('Salvar'),
+                    ),
+                  ],
+                ),
+              );
+            },
             child: const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -58,23 +120,14 @@ class GabaritoActions extends StatelessWidget {
                   title: Text('Apagar gabarito "${gabarito.nome}"?'),
                   content: const Text('Essa ação não pode ser desfeita'),
                   actions: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text('Cancelar'),
-                    ),
+                    cancelButton(context),
                     FilledButton(
                       onPressed: () {
-                        int count = 0;
-                        store.deleteGabarito(gabarito.id!).then((_) {
-                          Navigator.of(context).popUntil((_) => count++ >= 2);
-                        }).catchError((_) {
-                          store.errors.map((err) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text(err)),
-                            );
-                          });
-                          Navigator.of(context).pop();
-                        });
+                        handlePromise(
+                          store.deleteGabarito(gabarito.id!),
+                          context,
+                          store,
+                        );
                       },
                       style: deleteFilledButtonStyle,
                       child: const Text('Apagar'),
