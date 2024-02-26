bcc263abf328bfd3951024e83ccc01f7032214bb
Adriel <adriel.fsantos@outlook.com>
2024-02-25 22:43:15 -0300

    feat: Add illustration on Home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meu_gabarito/widgets/logo_appbar.dart';
 import 'package:meu_gabarito/widgets/new_test_template_floating_button.dart';
 import 'package:meu_gabarito/widgets/recent_gabaritos.dart';
@@ -8,13 +9,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: LogoAppBar(),
-      floatingActionButton: NewTestTemplateFloatingButton(),
+    return Scaffold(
+      appBar: const LogoAppBar(),
+      floatingActionButton: const NewTestTemplateFloatingButton(),
       body: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            RecentGabaritos(padding: EdgeInsets.all(16)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: SvgPicture.asset(
+                'assets/img/illustration-study.svg',
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+            ),
+            const RecentGabaritos(padding: EdgeInsets.all(16)),
           ],
         ),
       ),
