7f65100c6eaee26c9503a17952ade22a433dd78b
Adriel <adriel.fsantos@outlook.com>
2024-02-27 22:13:35 -0300

    feat: Add QuestaoCorrigidaInfo widget

---
+++ lib/widgets/questao_corrigida_info.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:meu_gabarito/classes/questao.dart';
+import 'package:meu_gabarito/enums/alternativa.dart';
+
+class QuestaoCorrigidaInfo extends StatelessWidget {
+  final Questao questao;
+
+  const QuestaoCorrigidaInfo(this.questao, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool correct =
+        questao.alternativaSelecionada! == questao.alternativaCorreta!;
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: FilledButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(
+                correct ? Colors.green : Colors.red,
+              ),
+            ),
+            onPressed: () {},
+            child: Text(
+              Alternativa.values[questao.alternativaSelecionada!.index].name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
