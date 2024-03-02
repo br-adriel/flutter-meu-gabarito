79f9ab3af2f4f3a36f56a5712e693eb0e089bc29
Adriel <adriel.fsantos@outlook.com>
2024-03-02 14:35:12 -0300

    feat: Update QuestaoCard to handle question correction

--- lib/widgets/questao_card.dart
+++ lib/widgets/questao_card.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:meu_gabarito/classes/questao.dart';
 import 'package:meu_gabarito/enums/alternativa.dart';
+import 'package:meu_gabarito/store/gabaritos/gabaritos.dart';
+import 'package:meu_gabarito/store/main.dart';
 import 'package:meu_gabarito/themes/styles/text_styles.dart';
 import 'package:meu_gabarito/widgets/questao_corrigida_info.dart';
 import 'package:meu_gabarito/widgets/questao_options.dart';
+import 'package:provider/provider.dart';
 
 class QuestaoCard extends StatelessWidget {
   final Questao questao;
@@ -12,41 +16,57 @@ class QuestaoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Gabaritos store = Provider.of<MainStore>(context).gabaritos;
+
     String? correctAlternative;
     if (questao.alternativaCorreta != null &&
         questao.alternativaCorreta != questao.alternativaSelecionada) {
       correctAlternative =
           Alternativa.values[questao.alternativaCorreta!.index].name;
     }
+
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Questão ${questao.numero.toString()}',
-                  style: title2TextStyle,
+        child: Observer(
+          builder: (context) => Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Questão ${questao.numero.toString()}',
+                    style: title2TextStyle,
+                  ),
                 ),
               ),
-            ),
-            correctAlternative != null
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('Correta: $correctAlternative'),
-                    ),
-                  )
-                : Container(),
-            const SizedBox(height: 8),
-            questao.corrigida == null || questao.corrigida! == false
-                ? QuestaoOptions(questao)
-                : QuestaoCorrigidaInfo(questao),
-          ],
+              correctAlternative != null
+                  ? Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Correta: $correctAlternative'),
+                      ),
+                    )
+                  : Container(),
+              store.isCorrectionModeEnabled && questao.corrigida == false
+                  ? const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 4),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Corrigindo: selecione a alternativa correta',
+                        ),
+                      ),
+                    )
+                  : Container(),
+              const SizedBox(height: 8),
+              questao.corrigida == null || questao.corrigida! == false
+                  ? QuestaoOptions(questao)
+                  : QuestaoCorrigidaInfo(questao),
+            ],
+          ),
         ),
       ),
     );
