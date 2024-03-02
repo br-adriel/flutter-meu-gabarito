eaf266f66a32af2f33fb8ee3aecb8a3d9d6cfd04
Adriel <adriel.fsantos@outlook.com>
2024-03-02 14:35:48 -0300

    feat: Update AlternativaButton to handle question correction

--- lib/widgets/alternativa_button.dart
+++ lib/widgets/alternativa_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:meu_gabarito/classes/questao.dart';
 import 'package:meu_gabarito/enums/alternativa.dart';
 import 'package:meu_gabarito/store/gabaritos/gabaritos.dart';
@@ -20,25 +21,40 @@ class AlternativaButton extends StatelessWidget {
   Widget build(BuildContext context) {
     Gabaritos store = Provider.of<MainStore>(context).gabaritos;
 
-    return FilledButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStatePropertyAll(
-          questao.alternativaSelecionada != null &&
-                  questao.alternativaSelecionada == alternative
-              ? primary
-              : Colors.grey[600],
+    void showSnackbarError(String err) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(err)),
+      );
+    }
+
+    return Observer(
+      builder: (context) => FilledButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(
+            !store.isCorrectionModeEnabled &&
+                    questao.alternativaSelecionada != null &&
+                    questao.alternativaSelecionada == alternative
+                ? primary
+                : Colors.grey[600],
+          ),
         ),
+        child: Text(alternative.name),
+        onPressed: () {
+          if (store.isCorrectionModeEnabled) {
+            store
+                .correctQuestao(
+                  questao.id!,
+                  questao.alternativaSelecionada,
+                  alternative,
+                )
+                .catchError((_) => store.errors.map(showSnackbarError));
+            return;
+          }
+          store
+              .setAlternativa(questao.id!, alternative)
+              .catchError((_) => store.errors.map(showSnackbarError));
+        },
       ),
-      child: Text(alternative.name),
-      onPressed: () {
-        store.setAlternativa(questao.id!, alternative).catchError((_) {
-          store.errors.map((err) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(err)),
-            );
-          });
-        });
-      },
     );
   }
 }
